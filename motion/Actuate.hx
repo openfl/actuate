b3b59b9e059e7da176e0ef0fb9718d98b998acd4
gitjoe <hoseyhosey@gmail.com>
2015-01-07 17:15:47 -0800

    isRunning and merge

--- motion/Actuate.hx
+++ motion/Actuate.hx
@@ -1,7 +1,4 @@
-﻿/**
- * @author Joshua Granick
- */
-package motion;
+﻿package motion;
 
 
 import haxe.ds.ObjectMap;
@@ -14,7 +11,6 @@ import motion.actuators.SimpleActuator;
 import motion.actuators.TransformActuator;
 import motion.easing.Expo;
 import motion.easing.IEasing;
-
 #if (flash || nme || openfl)
 import flash.display.DisplayObject;
 #end
@@ -25,10 +21,8 @@ import flash.display.DisplayObject;
 class Actuate {
 	
 	
-	public static var defaultActuator:Class <GenericActuator> = SimpleActuator;
 	public static var defaultActuator:Class<IGenericActuator> = SimpleActuator;
 	public static var defaultEase:IEasing = Expo.easeOut;
-	private static var targetLibraries:ObjectHash <Array <GenericActuator>> = new ObjectHash <Array <GenericActuator>> ();
 	private static var targetLibraries = new ObjectMap<Dynamic, Array<IGenericActuator>> ();
 	
 	
@@ -40,26 +34,22 @@ class Actuate {
 	 * @param	customActuator		A custom actuator to use instead of the default (Optional)
 	 * @return		The current actuator instance, which can be used to apply properties like onComplete or onUpdate handlers
 	 */
-	public static function apply (target:Dynamic, properties:Dynamic, customActuator:Class <GenericActuator> = null):IGenericActuator {
 	@:generic public static function apply<T> (target:T, properties:Dynamic, customActuator:Class<GenericActuator<T>> = null):GenericActuator<T> {
 		
 		stop (target, properties);
 		
 		if (customActuator == null) {
 			
-			customActuator = defaultActuator;
 			customActuator = cast defaultActuator;
 			
 		}
 		
-		var actuator = Type.createInstance (customActuator, [ target, 0, properties ]);
 		var actuator:GenericActuator<T> = Type.createInstance (customActuator, [ target, 0, properties ]);
 		actuator.apply ();
 		
 		return actuator;
 		
 	}
-	
 	public static function isRunning():Bool {
 		var result:Bool = false;
 		for (library in targetLibraries) {
@@ -68,6 +58,7 @@ class Actuate {
 		}
 		return result;
 	}
+	
 	#if (flash || nme || openfl)
 	
 	/**
@@ -86,12 +77,10 @@ class Actuate {
 	#end
 	
 	
-	private static function getLibrary (target:Dynamic, allowCreation:Bool = true):Array <GenericActuator> {
 	private static function getLibrary<T> (target:T, allowCreation:Bool = true):Array<IGenericActuator> {
 		
 		if (!targetLibraries.exists (target) && allowCreation) {
 			
-			targetLibraries.set (target, new Array <GenericActuator> ());
 			targetLibraries.set (target, new Array<IGenericActuator> ());
 			
 		}
@@ -109,7 +98,6 @@ class Actuate {
 	 * @param	overwrite		Sets whether previous tweens for the same target and properties will be overwritten (Default is true)
 	 * @return		The current actuator instance, which can be used to apply properties like ease, delay, onComplete or onUpdate
 	 */
-	public static function motionPath (target:Dynamic, duration:Float, properties:Dynamic, overwrite:Bool = true):IGenericActuator {
 	@:generic public static function motionPath<T> (target:T, duration:Float, properties:Dynamic, overwrite:Bool = true):GenericActuator<T> {
 		
 		return tween (target, duration, properties, overwrite, MotionPathActuator);
@@ -121,21 +109,16 @@ class Actuate {
 	 * Pauses tweens for the specified target objects
 	 * @param	... targets		The target objects which will have their tweens paused. Passing no value pauses tweens for all objects
 	 */
-	//public static function pause (... targets:Array):void {
-	public static function pause (target:Dynamic):Void {
 	//@:generic public static function pause (... targets:Array):void {
 	@:generic public static function pause<T> (target:T):Void {
 		
-		if (Std.is (target, GenericActuator)) {
 		if (Std.is (target, IGenericActuator)) {
 			
-			cast (target, GenericActuator).pause ();
 			var actuator:IGenericActuator = cast target;
 			actuator.pause ();
 			
 		} else {
 			
-			var library:Array <GenericActuator> = getLibrary (target, false);
 			var library = getLibrary (target, false);
 			
 			if (library != null) {
@@ -186,7 +169,6 @@ class Actuate {
 			
 		}
 		
-		targetLibraries = new ObjectHash <Array <GenericActuator>> ();
 		targetLibraries = new ObjectMap<Dynamic, Array<IGenericActuator>> ();
 		
 	}
@@ -196,19 +178,15 @@ class Actuate {
 	 * Resumes paused tweens for the specified target objects
 	 * @param	... targets		The target objects which will have their tweens resumed. Passing no value resumes tweens for all objects
 	 */
-	public static function resume (target:Dynamic):Void {
 	@:generic public static function resume<T> (target:T):Void {
 		
-		if (Std.is (target, GenericActuator)) {
 		if (Std.is (target, IGenericActuator)) {
 			
-			cast (target, GenericActuator).resume ();
 			var actuator:IGenericActuator = cast target;
 			actuator.resume ();
 			
 		} else {
 			
-			var library:Array <GenericActuator> = getLibrary (target, false);
 			var library = getLibrary (target, false);
 			
 			if (library != null) {
@@ -248,21 +226,17 @@ class Actuate {
 	 * @param	complete		If tweens should apply their final target values before stopping. Default is false (Optional) 
 	 * @param	sendEvent	If a complete() event should be dispatched for the specified target. Default is true (Optional)
 	 */
-	public static function stop (target:Dynamic, properties:Dynamic = null, complete:Bool = false, sendEvent:Bool = true):Void {
 	@:generic public static function stop<T> (target:T, properties:Dynamic = null, complete:Bool = false, sendEvent:Bool = true):Void {
 		
 		if (target != null) {
 			
-			if (Std.is (target, GenericActuator)) {
 			if (Std.is (target, IGenericActuator)) {
 				
-				cast (target, GenericActuator).stop (null, complete, sendEvent);
 				var actuator:IGenericActuator = cast target;
 				actuator.stop (null, complete, sendEvent);
 				
 			} else {
 				
-				var library:Array <GenericActuator> = getLibrary (target, false);
 				var library = getLibrary (target, false);
 				
 				if (library != null) {
@@ -312,10 +286,8 @@ class Actuate {
 	 * @param	customActuator		A custom actuator to use instead of the default (Optional)
 	 * @return		The current actuator instance, which can be used to apply properties like onComplete or to gain a reference to the target timer object
 	 */
-	public static function timer (duration:Float, customActuator:Class <GenericActuator> = null):IGenericActuator {
 	public static function timer (duration:Float, customActuator:Class<GenericActuator<TweenTimer>> = null):GenericActuator<TweenTimer> {
 		
-		return tween (new TweenTimer (0), duration, new TweenTimer (1), false, customActuator);
 		return cast tween (new TweenTimer (0), duration, new TweenTimer (1), false, cast customActuator);
 		
 	}
@@ -331,7 +303,6 @@ class Actuate {
 	 * @param	overwrite		Sets whether previous tweens for the same target and properties will be overwritten (Default is true)
 	 * @return		A TransformOptions instance, which is used to select the kind of transform you would like to apply to the target
 	 */
-	public static function transform (target:Dynamic, duration:Float = 0, overwrite:Bool = true):TransformOptions {
 	@:generic public static function transform<T> (target:T, duration:Float = 0, overwrite:Bool = true):TransformOptions<T> {
 		
 		return new TransformOptions (target, duration, overwrite);
@@ -351,7 +322,6 @@ class Actuate {
 	 * @param	customActuator		A custom actuator to use instead of the default (Optional)
 	 * @return		The current actuator instance, which can be used to apply properties like ease, delay, onComplete or onUpdate
 	 */ 
-	public static function tween (target:Dynamic, duration:Float, properties:Dynamic, overwrite:Bool = true, customActuator:Class <GenericActuator> = null):IGenericActuator {
 	@:generic public static function tween<T> (target:T, duration:Float, properties:Dynamic, overwrite:Bool = true, customActuator:Class<GenericActuator<T>> = null):GenericActuator<T> {
 		
 		if (target != null) {
@@ -360,12 +330,10 @@ class Actuate {
 				
 				if (customActuator == null) {
 					
-					customActuator = defaultActuator;
 					customActuator = cast defaultActuator;
 					
 				}
 				
-				var actuator = Type.createInstance (customActuator, [ target, duration, properties ]);
 				var actuator:GenericActuator<T> = Type.createInstance (customActuator, [ target, duration, properties ]);
 				var library = getLibrary (actuator.target);
 				
@@ -385,19 +353,6 @@ class Actuate {
 				library.push (actuator);
 				actuator.move ();
 				
-				/*var actuator:GenericActuator = createInstance (customActuator, target, duration, properties);
-				
-				if (overwrite) {
-					
-					stop (target, properties, false, false);
-					
-				}
-				
-				var library:Array <GenericActuator> = getLibrary (target);
-				library.push (actuator);
-				
-				actuator.move ();*/
-				
 				return actuator;
 				
 			} else {
@@ -413,7 +368,6 @@ class Actuate {
 	}
 	
 	
-	public static function unload (actuator:GenericActuator):Void {
 	@:generic public static function unload<T> (actuator:GenericActuator<T>):Void {
 		
 		var target = actuator.target;
@@ -443,8 +397,6 @@ class Actuate {
 	 * @param	overwrite		Sets whether previous tweens for the same target and properties will be overwritten (Default is true)
 	 * @return		The current actuator instance, which can be used to apply properties like ease, delay, onComplete or onUpdate
 	 */
-	public static function update (target:Dynamic, duration:Float, start:Array <Dynamic> = null, end:Array <Dynamic> = null, overwrite:Bool = true):IGenericActuator {
-		
 	@:generic public static function update<T> (target:T, duration:Float, start:Array <Dynamic> = null, end:Array <Dynamic> = null, overwrite:Bool = true):GenericActuator<T> {
 				
 		var properties:Dynamic = { start: start, end: end };
@@ -506,17 +458,14 @@ private class EffectsOptions {
 }
 
 
-private class TransformOptions {
 private class TransformOptions<T> {
 
 
 	private var duration:Float;
 	private var overwrite:Bool;
-	private var target:Dynamic;
 	private var target:T;
 
 
-	public function new (target:Dynamic, duration:Float, overwrite:Bool) {
 	public function new (target:T, duration:Float, overwrite:Bool) {
 		
 		this.target = target;
@@ -593,166 +542,4 @@ private class TweenTimer {
 	}
 
 
-}
-
-
-#if !haxe3
-
-
-#if flash
-import flash.utils.TypedDictionary;
-#end
-
-
-private class ObjectHash <T> {
-	
-	
-	#if flash
-	
-	private var dictionary:TypedDictionary <Dynamic, T>;
-	
-	#else
-	
-	private var hash:IntHash <T>;
-	
-	#end
-	
-	private static var nextObjectID:Int = 0;
-	
-	
-	public function new () {
-		
-		#if flash
-		
-		dictionary = new TypedDictionary <Dynamic, T> ();
-		
-		#else
-		
-		hash = new IntHash <T> ();
-		
-		#end
-		
-	}
-	
-	
-	public inline function exists (key:Dynamic):Bool {
-		
-		#if flash
-		
-		return dictionary.exists (key);
-		
-		#else
-		
-		return hash.exists (getID (key));
-		
-		#end
-		
-	}
-	
-	
-	public inline function get (key:Dynamic):T {
-		
-		#if flash
-		
-		return dictionary.get (key);
-		
-		#else
-		
-		return hash.get (getID (key));
-		
-		#end
-		
-	}
-	
-	
-	private inline function getID (key:Dynamic):Int {
-		
-		#if cpp
-		
-		return untyped __global__.__hxcpp_obj_id (key);
-		
-		#else
-		
-		if (key.___id___ == null) {
-			
-			key.___id___ = nextObjectID ++;
-			
-			if (nextObjectID == 0x3FFFFFFF) {
-				
-				nextObjectID = 0;
-				
-			}
-			
-		}
-		
-		return key.___id___;
-		
-		#else
-		
-		return 0;
-		
-		#end
-		
-	}
-	
-	
-	public inline function iterator ():Iterator <T> {
-		
-		#if flash
-		
-		var values:Array <T> = new Array <T> ();
-		
-		for (key in dictionary.iterator ()) {
-			
-			values.push (dictionary.get (key));
-			
-		}
-		
-		return values.iterator ();
-		
-		#else
-		
-		return hash.iterator ();
-		
-		#end
-		
-	}
-	
-	
-	public inline function remove (key:Dynamic):Void {
-		
-		#if flash
-		
-		dictionary.delete (key);
-		
-		#else
-		
-		hash.remove (getID (key));
-		
-		#end
-		
-	}
-	
-	
-	public inline function set (key:Dynamic, value:T):Void {
-		
-		#if flash
-		
-		dictionary.set (key, value);
-		
-		#else
-		
-		hash.set (getID (key), value);
-		
-		#end
-		
-	}
-	
-	
-}
-
-
-#else
-typedef ObjectHash<T> = haxe.ds.ObjectMap<Dynamic, T>;
-#end
 }
