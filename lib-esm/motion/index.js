ad935632e34cda9f379da17723bc98722909e04f
Andres Alva <andresa88@gmail.com>
2018-05-31 12:41:52 -0700

    Moved default Actuate export lines to the top of lib-esm/motion/index.js to deal with circular dependency issue

--- lib-esm/motion/index.js
+++ lib-esm/motion/index.js
@@ -1,5 +1,6 @@
-
+// We must export from ./Actaute at the top of this file to deal with circular dependency issue
+export { default as Actuate } from "./Actuate";
+export { default as default } from "./Actuate";
 export * from "./actuators";
 export * from "./easing";
-export { default as Actuate, default } from "./Actuate";
 export { default as MotionPath } from "./MotionPath";
