e3d54304eba02cccdd8dfe06a32175924feabf80
Josh Tynjala <joshtynjala@bowlerhat.dev>
2023-10-02 15:25:47 -0700

    npm: update dependencies and add to Github Actions

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,29 +1,29 @@
 const webpack = require ("webpack");
-const merge = require ("webpack-merge");
-const UglifyJSPlugin = require ("uglifyjs-webpack-plugin");
+const { merge } = require ("webpack-merge");
+const BrotliPlugin = require ('brotli-webpack-plugin');
+const CompressionPlugin = require ("compression-webpack-plugin");
 const common = require ("./webpack.common.js");
 const package = require ("./package.json");
 
 var banner = "/*! Actuate v" + package.version + " | MIT (c) Joshua Granick | https://github.com/jgranick/actuate */\n"
 
 module.exports = merge (common, {
+	mode: "production",
 	output: {
 		filename: "actuate.min.js"
 	},
 	plugins: [
-		new UglifyJSPlugin ({
-			sourceMap: true,
-			uglifyOptions: {
-				// warnings: true
-			}
-		}),
 		new webpack.BannerPlugin ({
 			banner: banner,
 			raw: true,
 			entryOnly: true
 		}),
-		new webpack.DefinePlugin ({
-			"process.env.NODE_ENV": JSON.stringify ("production")
+		new CompressionPlugin ({
+			include: /[A-Za-z0-9]*\.min\.js$/
+		}),
+		new BrotliPlugin ({
+			asset: '[path].br[query]',
+			test: /[A-Za-z0-9]*\.min\.js$/
 		})
 	]
 });
